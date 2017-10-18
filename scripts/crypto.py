cef1269f0da14c504664b30a70225bdc78744584
x90-nop <x90code@outlook.com>
2017-10-18 21:59:05 -0200

    Minor code optimization

--- scripts/crypto.py
+++ scripts/crypto.py
@@ -3,16 +3,12 @@
 import argparse
 import BeautifulSoup
 import requests
-import sys
-import os.path
 
 html_parse = '0123456789,.'
-BTC_URL = 'https://coinmarketcap.com/currencies/bitcoin/'
-ETH_URL = 'https://coinmarketcap.com/currencies/ethereum/'
-XMR_URL = 'https://coinmarketcap.com/currencies/monero/'
+URL = 'https://coinmarketcap.com/currencies/'
 
-def get_price(url):
-	html_source = requests.get(url)
+def get_price(url, currency_name):
+	html_source = requests.get(url+currency_name)
 	Soup = BeautifulSoup.BeautifulSoup
 	soup = Soup(html_source.content)
 	cryptocurrency = str(soup.find(id='quote_price'))
@@ -22,18 +18,9 @@ def get_price(url):
 			price  += c
 	return price;
 
-def coin_switch(coin):
-		if(coin=='BTC'):
-			return get_price(BTC_URL)
-		elif(coin=='ETH'):
-			return get_price(ETH_URL)
-		elif(coin=='XMR'):
-			return get_price(XMR_URL)
-
-
 parser = argparse.ArgumentParser(description='Provides price info about cryptocurrencies')
-parser.add_argument('-c', action = 'store', dest = 'currency', choices=['BTC', 'ETH', 'XMR'],
-	required = True, help = 'Cryptocurrency abreviation(BTC,ETH or XMR).')
+parser.add_argument('-c', action = 'store', dest = 'currency',
+	required = True, help = 'Cryptocurrency full name(bitcoin,monero...)')
 args = parser.parse_args()
 
-print coin_switch(args.currency)
+print get_price(URL, args.currency)
